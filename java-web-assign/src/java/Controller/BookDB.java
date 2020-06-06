e83b35bbf5b1099c43754f6789ab856ed15fccba
lamnehihi <thanhlamnguyenhuynh@gmail.com>
2020-06-06 09:44:00 +0700

    get by keyword book method'

--- java-web-assign/src/java/Controller/BookDB.java
+++ java-web-assign/src/java/Controller/BookDB.java
@@ -7,6 +7,7 @@ package Controller;
 
 import Model.*;
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -15,9 +16,10 @@ import java.util.logging.Logger;
  * @author Admin
  */
 public class BookDB implements DatabaseInfo {
+
     //--------------------------------------------------------------------
-    public static Book getBookById(String bid) {
-        Book b = null;  
+    public static Book getById(String bid) {
+        Book b = null;
         try {
             Class.forName(driverName);
             try (Connection con = DriverManager.getConnection(dbURL, userDB, passDB)) {
@@ -28,11 +30,12 @@ public class BookDB implements DatabaseInfo {
                     String bID = rs.getString(1);
                     String bName = rs.getString(2);
                     String catID = rs.getString(3);
-                    String bPrice = rs.getString(4);
-                    String bQuantity = rs.getString(5);
+                    float bPrice = Float.parseFloat(rs.getString(4));
+                    int bQuantity = Integer.parseInt(rs.getString(5));
                     String bAuthor = rs.getString(6);
                     String bDes = rs.getString(7);
                     String bCover = rs.getString(8);
+                    b = new Book(bID, bName, catID, bPrice, bQuantity, bAuthor, bDes, bCover);
                 }
                 con.close();
             }
@@ -44,29 +47,78 @@ public class BookDB implements DatabaseInfo {
     }
 
     //--------------------------------------------------------------------
-    public static Book getAllBook() {
+    public static ArrayList<Book> getAll() {
         Book b = null;
+        ArrayList<Book> bookList = new ArrayList<Book>();
         try {
             Class.forName(driverName);
             try (Connection con = DriverManager.getConnection(dbURL, userDB, passDB)) {
                 PreparedStatement stmt = con.prepareStatement("Select bID, bName, catID, bPrice, bQuantity, bAuthor, bDes, bCover from Books");
                 ResultSet rs = stmt.executeQuery();
-                if (rs.next()) {
+                while (rs.next()) {
                     String bID = rs.getString(1);
                     String bName = rs.getString(2);
                     String catID = rs.getString(3);
-                    String bPrice = rs.getString(4);
-                    String bQuantity = rs.getString(5);
+                    float bPrice = Float.parseFloat(rs.getString(4));
+                    int bQuantity = Integer.parseInt(rs.getString(5));
                     String bAuthor = rs.getString(6);
                     String bDes = rs.getString(7);
                     String bCover = rs.getString(8);
+                    b = new Book(bID, bName, catID, bPrice, bQuantity, bAuthor, bDes, bCover);
+                    bookList.add(b);
                 }
                 con.close();
             }
-            return b;
+            return bookList;
         } catch (ClassNotFoundException | SQLException e) {
             Logger.getLogger(BookDB.class.getName()).log(Level.SEVERE, null, e);
             return null;
         }
+    }
+
+    //--------------------------------------------------------------------
+    public static ArrayList<Book> getByKeyWord(String key) {
+        Book b = null;
+        ArrayList<Book> bookList = new ArrayList<Book>();
+        try {
+            Class.forName(driverName);
+            try (Connection con = DriverManager.getConnection(dbURL, userDB, passDB)) {
+                String sql = "Select bID, bName, catID, bPrice, bQuantity, bAuthor, bDes, bCover from Books ";
+                if (key.length() > 0) {
+                    sql += "where bName like '%" + key + "%' ";
+                }
+                Statement stmt = con.createStatement();
+                ResultSet rs = stmt.executeQuery(sql);
+                while (rs.next()) {
+                    String bID = rs.getString(1);
+                    String bName = rs.getString(2);
+                    String catID = rs.getString(3);
+                    float bPrice = Float.parseFloat(rs.getString(4));
+                    int bQuantity = Integer.parseInt(rs.getString(5));
+                    String bAuthor = rs.getString(6);
+                    String bDes = rs.getString(7);
+                    String bCover = rs.getString(8);
+                    b = new Book(bID, bName, catID, bPrice, bQuantity, bAuthor, bDes, bCover);
+                    bookList.add(b);
+                }
+                con.close();
+            }
+            return bookList;
+        } catch (ClassNotFoundException | SQLException e) {
+            Logger.getLogger(BookDB.class.getName()).log(Level.SEVERE, null, e);
+            return null;
+        }
+    }
+
+    public static void main(String[] args) {
+        Book b = new Book();
+        ArrayList<Book> bl = new ArrayList<>();
+        bl = BookDB.getByKeyWord("");
+        System.out.println(bl.size());
+        for (int i = 0; i < bl.size(); i++) {
+            System.out.println(bl.get(i).getbName());
+        }
+        b = BookDB.getById("B0001");
+        //System.out.println(b.getbName());
     }
 }
