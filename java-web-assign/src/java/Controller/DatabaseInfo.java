1ac960654fe1666229865f808297d666c84d76b3
lamnehihi <phucdoan252000@gmail.com>
2020-06-08 07:45:29 +0700

    AdminFunction

--- java-web-assign/src/java/Controller/DatabaseInfo.java
+++ java-web-assign/src/java/Controller/DatabaseInfo.java
@@ -3,6 +3,6 @@ package Controller;
 public interface DatabaseInfo {
     public static String driverName="com.microsoft.sqlserver.jdbc.SQLServerDriver";
     public static String dbURL="jdbc:sqlserver://localhost:1433;databaseName=BookWebsite;";
-    public static String userDB="phattp";
+    public static String userDB="phuc";
     public static String passDB="abcd";
 }
