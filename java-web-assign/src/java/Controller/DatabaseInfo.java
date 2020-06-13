e557cc4505bd0785761a0041fbdef6754832583b
lamnehihi <51854050+lamnehihi@users.noreply.github.com>
2020-06-13 09:34:14 +0700

    Update DatabaseInfo.java

--- java-web-assign/src/java/Controller/DatabaseInfo.java
+++ java-web-assign/src/java/Controller/DatabaseInfo.java
@@ -2,9 +2,7 @@ package Controller;
 
 public interface DatabaseInfo {
     public static String driverName="com.microsoft.sqlserver.jdbc.SQLServerDriver";
-
     public static String dbURL="jdbc:sqlserver://127.0.0.1:1433;databaseName=BookWebsite;";
-
     public static String userDB="tuenm";
     public static String passDB="mt10x1psn";
 
